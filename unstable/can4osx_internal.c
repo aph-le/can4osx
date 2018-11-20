e5ac479a14771a85046506cef2e43c79546d8ed9
aph-le <alexanderphilipp@gmx.net>
2018-11-20 16:42:05 +0100

    Changed to coding rules

--- unstable/can4osx_internal.c
+++ unstable/can4osx_internal.c
@@ -70,51 +70,51 @@ CanEventMsgBuf* CAN4OSX_CreateCanEventBuffer(
 	if ( bufferRef == NULL )  {
 		return(NULL);
 	}
-    
+
 	bufferRef->bufferSize = bufferSize;
 	bufferRef->bufferCount = 0;
 	bufferRef->bufferFirst = 0;
-    
+
 	bufferRef->canMsgRef = malloc(bufferSize * sizeof(CanMsg));
-    
+
 	if ( bufferRef->canMsgRef == NULL )  {
 		free(bufferRef);
-        bufferRef = NULL;
+		bufferRef = NULL;
 		return(NULL);
 	}
-    
-    bufferRef->bufferGDCqueueRef = dispatch_queue_create("com.can4osx.caneventqueue", 0);
-    if ( bufferRef->bufferGDCqueueRef == NULL )  {
+
+	bufferRef->bufferGDCqueueRef = dispatch_queue_create("com.can4osx.caneventqueue", 0);
+	if ( bufferRef->bufferGDCqueueRef == NULL )  {
 		CAN4OSX_ReleaseCanEventBuffer(bufferRef);
 		return(NULL);
 	}
-    
+
 	return(bufferRef);
 }
 
 
 /******************************************************************************/
 void CAN4OSX_ReleaseCanEventBuffer(
-        CanEventMsgBuf* bufferRef
-    )
+		CanEventMsgBuf* bufferRef
+	)
 {
-	if ( bufferRef != NULL ) {
-        if (bufferRef->bufferGDCqueueRef != NULL) {
-            dispatch_release(bufferRef->bufferGDCqueueRef);
-        }
+	if ( bufferRef != NULL )  {
+		if (bufferRef->bufferGDCqueueRef != NULL)  {
+			dispatch_release(bufferRef->bufferGDCqueueRef);
+		}
 		free(bufferRef->canMsgRef);
-		free(bufferRef);
-        
         bufferRef->canMsgRef = NULL;
-        bufferRef = NULL;
+        
+		free(bufferRef);
+		bufferRef = NULL;
 	}
 }
 
 
 /******************************************************************************/
 static UInt8 CAN4OSX_TestFullCanEventBuffer(
-        CanEventMsgBuf* bufferRef
-    )
+		CanEventMsgBuf* bufferRef
+	)
 {
 	if (bufferRef->bufferCount == bufferRef->bufferSize)  {
 		return(1);
@@ -126,55 +126,55 @@ static UInt8 CAN4OSX_TestFullCanEventBuffer(
 
 /******************************************************************************/
 static UInt8 CAN4OSX_TestEmptyCanEventBuffer(
-        CanEventMsgBuf* bufferRef
-    )
+		CanEventMsgBuf* bufferRef
+	)
 {
-    if ( bufferRef->bufferCount == 0 )  {
-        return(1);
-    } else {
-        return(0);
-    }
+	if ( bufferRef->bufferCount == 0 )  {
+		return(1);
+	} else {
+		return(0);
+	}
 }
 
 
 /******************************************************************************/
 UInt8 CAN4OSX_WriteCanEventBuffer(
-        CanEventMsgBuf* bufferRef,
-        CanMsg newEvent
-    )
+		CanEventMsgBuf* bufferRef,
+		CanMsg newEvent
+	)
 {
 __block UInt8 retval = 1;
-    
-    dispatch_sync(bufferRef->bufferGDCqueueRef, ^{
-        if (CAN4OSX_TestFullCanEventBuffer(bufferRef))  {
-            retval = 0;
-        } else {
-            bufferRef->canMsgRef[(bufferRef->bufferFirst + bufferRef->bufferCount++) % bufferRef->bufferSize] = newEvent;
-        }
-    });
-    
+
+	dispatch_sync(bufferRef->bufferGDCqueueRef, ^{
+		if (CAN4OSX_TestFullCanEventBuffer(bufferRef))  {
+			retval = 0;
+		} else {
+			bufferRef->canMsgRef[(bufferRef->bufferFirst + bufferRef->bufferCount++) % bufferRef->bufferSize] = newEvent;
+		}
+	});
+
 	return(retval);
 }
 
 
 /******************************************************************************/
 UInt8 CAN4OSX_ReadCanEventBuffer(
-        CanEventMsgBuf* bufferRef,
-        CanMsg* readEvent
-    )
+		CanEventMsgBuf* bufferRef,
+		CanMsg* readEvent
+	)
 {
 __block UInt8 retval = 1;
-    
-    dispatch_sync(bufferRef->bufferGDCqueueRef, ^{
-        if (CAN4OSX_TestEmptyCanEventBuffer(bufferRef))  {
-            retval = 0;
-        } else {
-            bufferRef->bufferCount--;
-            *readEvent = bufferRef->canMsgRef[bufferRef->bufferFirst++ % bufferRef->bufferSize];
-        }
 
-    });
-    
+	dispatch_sync(bufferRef->bufferGDCqueueRef, ^{
+		if (CAN4OSX_TestEmptyCanEventBuffer(bufferRef))  {
+			retval = 0;
+		} else {
+			bufferRef->bufferCount--;
+            *readEvent = bufferRef->canMsgRef[bufferRef->bufferFirst++ % bufferRef->bufferSize];
+		}
+
+	});
+
 	return(retval);
 
 }
@@ -182,36 +182,36 @@ __block UInt8 retval = 1;
 
 /******************************************************************************/
 canStatus CAN4OSX_GetChannelData(
-        Can4osxUsbDeviceHandleEntry* pSelf,
-        SInt32 cmd,
-        void* pBuffer,
-        size_t bufsize
-    )
+		Can4osxUsbDeviceHandleEntry* pSelf,
+		SInt32 cmd,
+		void* pBuffer,
+		size_t bufsize
+	)
 {
 
-    switch(cmd) {
-        case canCHANNELDATA_CARD_SERIAL_NO:
-            if (bufsize < 8)  {
-                return(canERR_NOMEM);
-            }
-            memcpy(pBuffer, &(pSelf->devInfo.serialNumber), 8);
-            break;
-        case canCHANNELDATA_DEVDESCR_ASCII:
-            if (bufsize > 128)  {
-                bufsize = 128;
-            }
-            memcpy(pBuffer, pSelf->devInfo.deviceString, bufsize);
-            break;
-        case canCHANNELDATA_CHANNEL_CAP:
-            if (bufsize < 4)  {
-                return(canERR_NOMEM);
-            }
-            memcpy(pBuffer, &(pSelf->devInfo.capability), 4);
-            break;
-        default:
-            break;
-    }
-    return(canOK);
+	switch(cmd) {
+		case canCHANNELDATA_CARD_SERIAL_NO:
+			if (bufsize < 8)  {
+				return(canERR_NOMEM);
+			}
+			memcpy(pBuffer, &(pSelf->devInfo.serialNumber), 8);
+			break;
+		case canCHANNELDATA_DEVDESCR_ASCII:
+			if (bufsize > 128)  {
+				bufsize = 128;
+			}
+			memcpy(pBuffer, pSelf->devInfo.deviceString, bufsize);
+			break;
+		case canCHANNELDATA_CHANNEL_CAP:
+			if (bufsize < 4)  {
+				return(canERR_NOMEM);
+			}
+			memcpy(pBuffer, &(pSelf->devInfo.capability), 4);
+			break;
+		default:
+			break;
+	}
+	return(canOK);
 }
 
 
@@ -222,12 +222,12 @@ canStatus CAN4OSX_GetChannelData(
 * \return the data length code
 */
 UInt8 CAN4OSX_decodeFdDlc(
-        UInt8 dlc
-    )
+		UInt8 dlc
+	)
 {
 static const UInt8 len[16u] = {0u,1u,2u,3u,4u,5u,6u,7u,8u,12u,16u,20u,24u,32u,48u,64u};
 
-    return(len[dlc]);
+	return(len[dlc]);
 }
 
 
@@ -238,36 +238,36 @@ static const UInt8 len[16u] = {0u,1u,2u,3u,4u,5u,6u,7u,8u,12u,16u,20u,24u,32u,48
 * \return the datalength
 */
 UInt8 CAN4OSX_encodeFdDlc(
-        UInt8 dlc
-    )
+		UInt8 dlc
+	)
 {
 	if (dlc == 12u)  {
 		return(0x09);
-    }
-    if (dlc == 16u)  {
-        return(0x0A);
-    }
-    if (dlc == 20u)  {
-        return(0x0B);
-    }
-    if (dlc == 24u)  {
-        return(0x0C);
-    }
-    if (dlc == 32u)  {
-        return(0x0D);
-    }
-    if (dlc == 48u)  {
-        return(0x0E);
-    }
-    if (dlc == 64u)  {
-        return(0x0F);
-    }
+	}
+	if (dlc == 16u)  {
+		return(0x0A);
+	}
+	if (dlc == 20u)  {
+		return(0x0B);
+	}
+	if (dlc == 24u)  {
+		return(0x0C);
+	}
+	if (dlc == 32u)  {
+		return(0x0D);
+	}
+	if (dlc == 48u)  {
+		return(0x0E);
+	}
+	if (dlc == 64u)  {
+		return(0x0F);
+	}
     /* invalid dlc */
-    if (dlc > 8u)  {
-    	return(0xff);
-    }
+	if (dlc > 8u)  {
+		return(0xff);
+	}
 
-    return(dlc);
+	return(dlc);
 }
 
 
@@ -279,11 +279,11 @@ UInt8 CAN4OSX_encodeFdDlc(
 */
 UInt64 CAN$OSX_getMilliseconds(
 		void
-    )
+	)
 {
 struct timeval time;
 
 	gettimeofday(&time, NULL);
-	return(time.tv_sec * 1000) + (time.tv_usec / 1000);
+    return(time.tv_sec * 1000) + (time.tv_usec / 1000);
 }
 
